7c931578aafbdb898f02cfcd13f3ad503a3a128c
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-12-19 12:21:12 -0500

    added fib solutions

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,40 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+function fib(n) {
+  if (n <= 2) return 1;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+// O(2n) => exponential
+
+
+//Memoization solution
+function fib2(n) {
+  let memo = [];
+  let result;
+  if (memo[n]) return memo[n];
+
+  if (n <= 2) result = 1;
+  else result = fib2(n - 1) + fib2(n - 2);
+
+  memo[n] = result;
+  return result;
+}
+//Time complexity O(n)
+
+//Bottom-Up approach
+function fib3(n) {
+  if (n <= 2) return 1;
+
+  let results = [];
+  results[1] = 1;
+  results[2] = 1;
+
+  for (let i = 3; i <= n; i++) {
+    results[i] = results[i - 1] + results[i - 2];
+  }
+  return results[n];
+}
+//Time complexity O(n)
--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,11 +1,40 @@
 //1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+}
+// whileLoop(5);
 
 //2. Next, try looping just like above except using recursion
 
+function recurseFromNum(n) {
+  if (n <= 0) return;
+
+  console.log(n);
+  return recurseFromNum(n - 1);
+}
+
+// recurseFromNum(5);
+
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+function exponent(base, expo) {
+  let sum = base;
+  let times = 1;
+  while (times < expo) {
+    sum *= base;
+    times++;
+  }
+  console.log(sum);
+}
+
+// exponent(5, 3);
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
 
+
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+
 
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
