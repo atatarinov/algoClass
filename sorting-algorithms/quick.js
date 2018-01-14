e782eb117a1a634cfc0a03e189280ab80def03b2
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-01-14 13:34:57 -0500

    added quicksort

--- sorting-algorithms/quick.js
+++ sorting-algorithms/quick.js
@@ -21,3 +21,48 @@ Variants:
 - Implement a multi-pivot quicksort (ex: partition into 3 subarrays using 2 pivots)
 
 */
+
+//different quicksort implementations
+const quickSort = (arr) => {
+  if (arr.length < 2) return arr;
+
+  let less = [];
+  let more = [];
+  let pivot = arr[arr.length - 1];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      less.push(arr[i]);
+    } else {
+      more.push(arr[i]);
+    }
+  }
+  return [...quickSort(less), pivot, ...quickSort(more)];
+};
+
+// average case O(n log n), worst case O(n^2)
+// choosing a random pivot each time, tends to make quicksort perform at the average case
+
+// *******************************************************
+const swap = (arr, i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+const partition = (arr) => {
+  let pivot = arr.length - 1;
+  let i = -1;
+  let j = 0;
+  while (j < pivot) {
+
+    if (arr[j] < arr[pivot]) {
+      i++;
+      swap(arr, i, j);
+    }
+    j++;
+  }
+  swap(arr, i + 1, pivot);
+
+  return arr;
+};
