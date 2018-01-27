1782161b403ce83f54613dad36c9c365548f31e5
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-01-26 23:34:04 -0500

    added optimal fib solution

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,36 +1,25 @@
 'use strict';
-/*
 
-Write a function that outputs the nth Fibonnaci number. A number in this sequence is found by adding up the two numbers before it.
-
-Fibonnaci's sequence:
-input    0 1 2 3 4 5 6  7  8  9 ...
-output   0 1 1 2 3 5 8 13 21 34 ...
-
-What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
-*/
 const fib = (n) => {
   if (n <= 2) return 1;
-
   return fib(n - 1) + fib(n - 2);
 };
 
 // O(2n) => exponential
+//O(2^n) and O(n) space
 
 
-//Memoization solution
-const fib2 = (n) => {
-  let memo = [];
-  let result;
-  if (memo[n]) return memo[n];
+//Memoization solution (this solution counts 0 as the fitst Fib number)
+const fib2 = (n, memoize = {1: 0, 2: 1}) => {
 
-  if (n <= 2) result = 1;
-  else result = fib2(n - 1) + fib2(n - 2);
+  if (n in memoize) return memoize[n];
 
-  memo[n] = result;
+  let result = fib2(n - 1) + fib2(n - 2);
+  memoize[n] = result;
+
   return result;
 };
-//Time complexity O(n)
+//Time complexity O(n), space O(n)
 
 //Bottom-Up approach
 const fib3 = (n) => {
@@ -46,3 +35,18 @@ const fib3 = (n) => {
   return results[n];
 };
 //Time complexity O(n)
+
+/****Optimal Space and Time Complexity***
+  O(n) Time and O(1) Space             */
+
+const fib4 = (n) => {
+  const lastTwo = [0, 1];
+  let counter = 3;
+  while (counter <= n) {
+    let nextNum = lastTwo[0] + lastTwo[1];
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = nextNum;
+    counter++;
+  }
+  return n > 1 ? lastTwo[1] : lastTwo[0];
+};
